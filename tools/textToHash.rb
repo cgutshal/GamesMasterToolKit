9dea6ff14d528537d57fc60f2019d0203306ec01
Caleb Gutshall <cgutshal@gmail.com>
2014-03-13 00:46:31 -0500

    need the keys to be ranges, they are currently strings

--- tools/textToHash.rb
+++ tools/textToHash.rb
@@ -7,15 +7,16 @@
 contents = File.read('weapons.txt')
 items = {}
 current_key = nil
-contents.strip.gsub(/\u2028/," ").spilt(' ').each do |item|
+contents.strip.gsub(/\u2028/," ").split(' ').each do |item|
   
-  if item[0] =~ /[[:digit;]]/
+  if item[0] =~ /[[:digit:]]/
     current_key = item.gsub(/\-/,"..")
+
   else
-    if item.has_key?(current_key)
-      items[current_key] = items[current_key] + " " + item
+    if items.has_key?(current_key)
+      items[current_key] = items[current_key] + " " +item
     else
-      itmes[current_key] = item
+      items[current_key] = item
     end
   end
 end
